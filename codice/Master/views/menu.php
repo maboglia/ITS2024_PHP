d59e6fa40e0109bab2957cb32a50acbce98ce77e
Mauro Bogliaccino <m.bogliaccino@studenti.local>
2024-06-13 10:24:53 +0200

    master

---
+++ codice/Master/views/menu.php
@@ -0,0 +1,3 @@
+
+<a href="?page=home" class="" title="">Home</a>
+<a href="?page=gallery" class="" title="">Gallery</a>
---
+++ codice/Master/views/pages/home.php
@@ -0,0 +1,4 @@
+<div id="home">
+    <h2 class="titolo">Home</h2>
+    <div><img src="https://img.pokemondb.net/artwork/avif/charizard.avif" alt="" class="foto_copertina"></div>
+</div>
