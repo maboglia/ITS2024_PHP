b9c42872c743ee1e15c10d68c7c1d501d46c1cb8
Mauro Bogliaccino <m.bogliaccino@studenti.local>
2024-07-18 09:55:11 +0200

    agg

---
+++ codice/Lez10/config.php
@@ -0,0 +1,17 @@
+<?php
+
+define('HOST', 'localhost');
+define('USER', 'root');
+define('PASS', '');
+define('DB_NAME', 'webdev');
+
+$connessione = mysqli_connect(HOST, USER, PASS, DB_NAME);
+
+if ($connessione){
+    echo 'SEI CONNESSO';
+} else {
+    echo 'NON SEI CONNESSO PERCHE\' ' . mysqli_error($connessione);
+}
+
+
+?>
